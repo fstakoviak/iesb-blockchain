1c88f04e26e168f1418555dc37a2435bfc020d5e
Lucas Vieira <--replace-all>
2019-06-24 12:20:29 -0300

    add contractAddress exportation

--- webapp/utils/parityRequests.js
+++ webapp/utils/parityRequests.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const allAccountsInfoRequest = { 
     "method": "parity_allAccountsInfo", 
     "params": [], 
@@ -7,6 +9,9 @@ const allAccountsInfoRequest = {
 
 const headers = { 'Content-Type': 'application/json' };
 const ownerAccount = "0x00a1103c941fc2e1ef8177e6d9cc4657643f274b";
+const MyContractJson = require(path.resolve('../dapp/build/contracts/MyContract.json'));
+const network = MyContractJson['networks']
+const contractAddress = network['8995'].address;
 
 function newAccountRequest(name, pass) {
     let newAccountRequest = { "method": "parity_newAccountFromPhrase", "params": [name, pass], "id": 1, "jsonrpc": "2.0" };
@@ -25,5 +30,6 @@ module.exports = {
     newAccountRequest,
     setAccountNameRequest,
     headers,
-    ownerAccount
+    ownerAccount,
+    contractAddress
 }
